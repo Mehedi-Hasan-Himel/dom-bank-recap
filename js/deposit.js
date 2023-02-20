b00e0c4434f39d7a94aa35034c5c410d4b6fae3a
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-20 20:01:48 +0600

    [ADD] Deposit set and balance set done

--- js/deposit.js
+++ js/deposit.js
@@ -5,6 +5,8 @@
 3. clear the deposit input field after getting value.
 4. get the previou deposit amount from the deposit total.
 5. calculate deposit total and set the value to the deposit total.
+6. get current balance.
+7. calculate new balance and set the value to the balance total.
 */
 
 // step-01
@@ -25,4 +27,15 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
    // step-05
    const newDepositTotal = previousDepositTotal + newDepositAmount;
    depositTotalElement.innerText = newDepositTotal;
+
+   // step-06
+   const balanceTotalElement = document.getElementById("balance-total");
+   const previousBalanceTotalString = balanceTotalElement.innerText
+   const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
+   
+
+   // step-07
+   const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+   balanceTotalElement.innerText = newBalanceTotal;
+   
 });
